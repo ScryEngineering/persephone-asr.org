6e545e77c76d622a9477a075ad7aff6a3144afd0
Janis <janis@customprogrammingsolutions.com>
2018-11-30 10:29:01 +1100

    Update copy about automated transcription workflows

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,10 @@ class App extends React.Component {
               Transcribe your audio into phonemes with an intuitive graphical tool that leverages the power of machine learning.
             </Header>
             <p className="masthead_sub">Persephone is a powerful open source toolkit for streamlining audio transcription processes.</p>
-            <p className="masthead_sub">Persephone is used by linguists in leading research institutions around the world, including North America, Europe, and the Asia-Pacific region. The tool saves these teams hundreds of hours of work every year by automating the laborous process of transcribing audio recordings.</p>
+            <p className="masthead_sub">
+              Persephone is used by linguists in leading research institutions around the world, including North America, Europe, and the Asia-Pacific region.
+              The tool allows these teams to automate the laborious process of transcribing audio recordings.
+            </p>
             <Grid stackable={true} centered={true} columns="3" className="illustration">
               <Grid.Column textAlign="center">
                 <Image src="/img/white-waveform.png" />
